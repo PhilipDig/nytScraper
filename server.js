eb319060c536bed7985de21c175a128603019feb
Philip Digiglio <37520782+PhilipDig@users.noreply.github.com>
2018-09-22 23:38:59 -0700

    Redeploy to heroku

--- server.js
+++ server.js
@@ -31,6 +31,7 @@ app.use(express.static("public"));
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
 // Set mongoose to leverage built in JavaScript ES6 Promises
+
 // Connect to the Mongo DB
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI);
